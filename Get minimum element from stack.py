94cca58c0bdeef453d5cd74b654daa3c567ec777
anirb <anirbang324@gmail.com>
2022-12-03 11:29:28 +0530

    stack related questions from gfg

---
+++ Get minimum element from stack.py
@@ -0,0 +1,59 @@
+# question link : https://practice.geeksforgeeks.org/problems/get-minimum-element-from-stack/1?page=1&category[]=Stack&sortBy=submissions
+
+# User function Template for python3
+
+class stack:
+    def __init__(self):
+        self.s = []
+        self.minEle = None
+
+    def push(self, x):
+        # CODE HERE
+        return self.s.append(x)
+
+    def pop(self):
+        # CODE HERE
+        if len(self.s) == 0:
+            return -1
+        return self.s.pop()
+
+    def getMin(self):
+        # CODE HERE
+        if len(self.s) == 0:
+            return -1
+
+        else:
+            min1 = min(self.s)
+            return min1
+
+
+# {
+# Driver Code Starts
+# contributed by RavinderSinghPB
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        q = int(input())
+
+        arr = [int(x) for x in input().split()]
+
+        stk = stack()
+
+        qi = 0
+        qn = 1
+        while qn <= q:
+            qt = arr[qi]
+
+            if qt == 1:
+                stk.push(arr[qi + 1])
+                qi += 2
+            elif qt == 2:
+                print(stk.pop(), end=' ')
+                qi += 1
+            else:
+                print(stk.getMin(), end=' ')
+                qi += 1
+            qn += 1
+        print()
+
+# } Driver Code Ends
---
+++ SortedStack.java
@@ -0,0 +1,50 @@
+// question link : https://practice.geeksforgeeks.org/problems/sort-a-stack/1?page=1&category[]=Stack&sortBy=submissions
+
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.Stack;
+class SortedStack{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Stack<Integer> s=new Stack<>();
+			int n=sc.nextInt();
+			while(n-->0)
+			s.push(sc.nextInt());
+			GfG g=new GfG();
+			Stack<Integer> a=g.sort(s);
+			while(!a.empty()){
+				System.out.print(a.peek()+" ");
+				a.pop();
+			}
+			System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class GfG{
+	public Stack<Integer> sort(Stack<Integer> s)
+	{
+		//add code here.
+		if(s.isEmpty()){
+		    return s;
+		}
+		int num = s.pop();
+		sort(s);
+		helper(s,num);
+		return s;
+	}
+	public void helper(Stack<Integer>s,int num){
+	    if(s.isEmpty()||s.peek()<num){
+	        s.add(num);
+	        return;
+	    }
+	    int n = s.pop();
+	    helper(s,num);
+	    s.add(n);
+	}
+}
