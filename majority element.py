c14f4456c0e43128c1c3eed6f7a0e01621eeaaa7
anirb <anirbang324@gmail.com>
2022-12-13 20:52:29 +0530

    geeksforgeeks - array related questions

--- majority element.py
+++ majority element.py
@@ -1,20 +1,44 @@
-from typing import List
+# User function template for Python 3
+
+class Solution:
+    def majorityElement(self, A, N):
+        if N == 1:
+            return A[0]
+        A.sort()
+        k = 1
+        for i in range(len(A) - 1):
+            if A[i] == A[i + 1]:
+                k = k + 1
+            else:
+                k = 1
+            if k > N // 2:
+                return A[i]
+
+        return -1
 
 
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        dct = {}
-        for i in nums:
-            dct[i] = dct.get(i, 0) + 1
-        for i in nums:
-            if dct[i] > (len(nums) // 2):
-                return i
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+import math
+
+from sys import stdin
 
 
-s = Solution()
-answer = s.majorityElement([3, 2, 3])
-print(answer)
+def main():
+    T = int(input())
+    while (T > 0):
+        N = int(input())
+
+        A = [int(x) for x in input().strip().split()]
+
+        obj = Solution()
+        print(obj.majorityElement(A, N))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
