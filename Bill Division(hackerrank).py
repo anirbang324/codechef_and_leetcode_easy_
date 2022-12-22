27a03c57c6a4343003291ee349876c64a002c153
anirb <anirbang324@gmail.com>
2022-12-22 16:01:21 +0530

    hackerrank problem solving questions

---
+++ Bill Division(hackerrank).py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    x = sum(bill) - bill[k]
+    if 2*b == x:
+        print("Bon Appetit")
+    else:
+        print(bill[k]//2)
+    
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
