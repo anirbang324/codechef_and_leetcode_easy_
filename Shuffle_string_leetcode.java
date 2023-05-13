3077564c7422c9134ab5985540fbb8b50b692b8a
anirb <anirbang324@gmail.com>
2023-05-13 21:29:34 +0530

    problem_solving_13.05.2023

---
+++ Shuffle_string_leetcode.java
@@ -0,0 +1,14 @@
+public class Shuffle_string_leetcode {
+    public String restoreString(String s, int[] indices) {
+        char temp[] = new char[indices.length];
+        for(int i=0;i<indices.length;i++){
+            temp[indices[i]] = s.charAt(i);
+        }
+        return new String(temp);
+    }
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] arr = new arr[4,5,6,7,0,2,1,3];
+        System.out.println(restoreString(s,arr));
+    }
+}
