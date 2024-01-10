e55d282b841295e046a34f9593e3b4e59bf75964
Anirban Goswami <60977714+anirbang324@users.noreply.github.com>
2024-01-10 23:15:39 +0530

    Update 56273435.py
    added comments to understand the logic.

--- 56273435.py
+++ 56273435.py
@@ -1,9 +1,12 @@
-# cook your dish here
-n=int(input())
-for i in range(0,n):
-    len = int(input())
-    str= input()
-    if(str[-1] in str[:len-1]):
+n = int(input())  # Read an integer 'n' from the user
+
+# Iterate 'n' times
+for i in range(0, n):
+    length = int(input())  # Read an integer 'length' from the user
+    string = input()  # Read a string 'str' from the user
+
+    # Check if the last character of the string is present in the rest of the string
+    if string[-1] in string[:length-1]:
         print("YES")
     else:
         print("NO")
